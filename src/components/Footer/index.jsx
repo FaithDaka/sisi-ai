639ea19cdea61ee77770f4042ea271660898a886
FaithDaka <faithbusigu8@gmail.com>
2024-01-11 16:31:13 +0300

    feat: V1 Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div className='footer-section'>
+        <div className='content'>
+            <div className='left-section'>
+                <div className='logo'>SISI AI</div>
+                <div className='subtitle-text'></div>
+                <div className='copyright'></div>
+            </div>
+            <div className='middle-section'>
+                <img src="" alt="" />
+            </div>
+            <div className='right-section'></div>
+        </div>
+    </div>
+  )
+}
+
+export default FooterComponent;
