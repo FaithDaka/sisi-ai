3105baefa220317477f490c7c2ba640987b27832
FaithDaka <faithbusigu8@gmail.com>
2024-01-11 16:30:54 +0300

    feat: Topbar component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const HeaderComponent = () => {
+  return (
+    <div className="sisi-header">
+      <div className="top-bar">
+        <div className="logo">SISI AI</div>
+        <nav className="nav">
+          <div>Home</div>
+          <div>About</div>
+          <div>Product</div>
+          <div>Contact</div>
+        </nav>
+      </div>
+      <div className="hero-section">
+        <div className="content">
+          <div className="title-text">SISI AI</div>
+          <div className="subtitle-text">
+            <div>UNLEASHING THE</div>
+            <div className="font-weight-500">
+              PAN-AFRICAN <span className="font-weight-700">AI</span>
+            </div>
+            <div>REVOLUTION</div>
+          </div>
+          <div className="cta-button">
+            <button className="button">Learn more</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderComponent;
