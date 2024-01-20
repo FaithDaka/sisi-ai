a42c4c267dd6a4a6318c0bfbd5d6e376e59329f8
FaithDaka <faithbusigu8@gmail.com>
2024-01-20 14:47:50 +0300

    refactor: homepage and topbar styling

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,24 +1,27 @@
-import AboutSectionComponent from 'components/Homepage/About';
-import FeaturesComponent from 'components/Homepage/Features';
-import HeaderComponent from 'components/Header';
-import MissionComponent from 'components/Homepage/Mission';
-import ProductComponent from 'components/Homepage/Product';
-import RevolutionComponent from 'components/Homepage/Revolution';
-import React from 'react'
-import FooterComponent from 'components/Footer';
+import AboutSectionComponent from "components/Homepage/About";
+import FeaturesComponent from "components/Homepage/Features";
+import HeaderComponent from "components/Header";
+import MissionComponent from "components/Homepage/Mission";
+import ProductComponent from "components/Homepage/Product";
+import RevolutionComponent from "components/Homepage/Revolution";
+import React from "react";
+import PageComponent from "components/Layout/PageComponent";
 
 const HomePage = () => {
   return (
-    <div className='Page home-page'>
-      <HeaderComponent/>
-      <MissionComponent/>
-      <AboutSectionComponent/>
-      <RevolutionComponent/>
-      <ProductComponent/>
-      <FeaturesComponent/>
-      <FooterComponent/>
-    </div>
-  )
-}
+    <>
+    <PageComponent>
+      <div className="home-page">
+        <HeaderComponent />
+        <MissionComponent />
+        <AboutSectionComponent />
+        <RevolutionComponent />
+        <ProductComponent />
+        <FeaturesComponent />
+      </div>
+      </PageComponent>
+    </>
+  );
+};
 
 export default HomePage;
